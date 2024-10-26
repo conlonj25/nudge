4ca2ab827c83ac8b94406e0948efc03675778c05
James Conlon <conlonj25@gmail.com>
2024-10-26 17:31:00 +0200

    move session to main layout

--- src/app/habits/page.tsx
+++ src/app/habits/page.tsx
@@ -1,21 +1,5 @@
-import { NavBarComponent } from "~/components/nav-bar";
-import { HydrateClient } from "~/trpc/server";
-import { getServerAuthSession } from "~/server/auth";
 import { MyHabits } from "../_components/my-habits";
 
-export default async function Home() {
-  const session = await getServerAuthSession();
-
-  return (
-    <HydrateClient>
-      <NavBarComponent session={session} />
-      {session && (
-        <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-pink-400 to-white to-90%">
-          <div className="container flex flex-col items-center justify-center gap-12 p-8">
-            <MyHabits />
-          </div>
-        </main>
-      )}
-    </HydrateClient>
-  );
+export default async function Page() {
+  return <MyHabits />;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { TRPCReactProvider } from "~/trpc/react";
+import { NavBarComponent } from "~/components/nav-bar";
+import { getServerAuthSession } from "~/server/auth";
+import { HydrateClient } from "~/trpc/server";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -10,13 +13,24 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const session = await getServerAuthSession();
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <HydrateClient>
+            <NavBarComponent session={session} />
+            <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-pink-400 to-white to-90%">
+              <div className="container flex flex-col items-center justify-center gap-12 p-8">
+                {session ? children : <h1>Please Sign In</h1>}
+              </div>
+            </main>
+          </HydrateClient>
+        </TRPCReactProvider>
       </body>
     </html>
   );
