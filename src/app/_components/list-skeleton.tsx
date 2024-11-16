10df56b8d6f0fcb0a7897f7b5b867e5c32e2a7f1
James Conlon <conlonj25@gmail.com>
2024-11-16 19:13:39 +0100

    fix skeletons to use brand colors

--- src/app/_components/list-skeleton.tsx
+++ src/app/_components/list-skeleton.tsx
@@ -3,11 +3,11 @@ import { Skeleton } from "~/components/ui/skeleton";
 const ListSkeleton = () => {
   return (
     <div className="flex flex-col items-stretch gap-4">
-      <Skeleton className="h-10 rounded-md bg-pink-50" />
+      <Skeleton className="h-10 rounded-md bg-primary" />
       <hr />
-      <Skeleton className="h-10 rounded-md bg-pink-50" />
+      <Skeleton className="h-10 rounded-md bg-primary" />
       <hr />
-      <Skeleton className="h-10 rounded-md bg-pink-50" />
+      <Skeleton className="h-10 rounded-md bg-primary" />
       <hr />
     </div>
   );
